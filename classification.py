a9129871bb840aeea8996b0495096cc57ca11672
Luc Ravenelle <lrav35@gmail.com>
2023-11-27 09:18:45 -0500

    reorginizing code for easier training runs

--- classification.py
+++ classification.py
@@ -1,4 +1,197 @@
-# model code
+import json
+import pandas as pd
+import matplotlib.pyplot as plt
+import torch
+from torch.utils.data import Dataset, DataLoader
+from torch.optim import AdamW
+from transformers import AutoModelForSequenceClassification, AutoTokenizer, get_scheduler
+from tqdm.auto import tqdm
+import evaluate
+import uuid
 
-if __name__ == 'main':
-    print('hello world')
+
+
+class ReviewDataSet(Dataset):
+    """ torch Dataset for our review data """
+    def __init__(self, tokenizer, dataframe):
+        self.tokenizer = tokenizer
+        self.data = self.format_dataset(dataframe)
+        self.size = len(dataframe)
+        
+
+    """gives length of dataset"""
+    def __len__(self) -> int:
+        return self.size
+
+    """function allows object to be subscriptable -> dataset[0]"""
+    def __getitem__(self, idx: int):
+        label = self.data['labels'][idx]
+        input_ids = self.data['input_ids'][idx]
+        token_type = self.data['token_type_ids'][idx]
+        att_mask = self.data['attention_mask'][idx]
+        sample = {'labels': label, 'input_ids': input_ids, 'token_type_ids': token_type, 'attention_mask': att_mask}
+        return sample
+
+
+    def tokenize_text(self, text_data):
+        token_dict = self.tokenizer(text_data, padding='max_length', truncation=True)
+        input = torch.tensor(token_dict['input_ids'])
+        token_type = torch.tensor(token_dict['token_type_ids'])
+        att_mask = torch.tensor(token_dict['attention_mask'])
+        return input, token_type, att_mask
+
+    def format_dataset(self, dataframe: pd.DataFrame):
+        input, token_type, att_mask = zip(*dataframe['text'].apply(self.tokenize_text))
+        # self.data['labels'] = torch.tensor(dataframe['stars'].to_list())-1
+        # self.data['input_ids'] = input
+        # self.data['token_type_ids'] = token_type
+        # self.data['attention_mask'] = att_mask
+        self.data = {
+            'labels': torch.tensor(dataframe['stars'].to_list())-1,
+            'input_ids': input,
+            'token_type_ids': token_type,
+            'attention_mask': att_mask
+        }
+        return self.data
+
+
+def load_and_prepare_data(path: str, COLAB: bool) -> pd.DataFrame:
+    if COLAB:
+        from google.colab import drive
+        drive.mount('/content/drive')
+
+    # read data
+    data = []
+    with open(path, "r") as f:
+        for i, line in enumerate(f):
+            if i >= 75000:
+                break
+            data.append(json.loads(line))
+    df = pd.DataFrame(data)
+
+    # create dataframe
+    review_df = df[['stars', 'text']].astype({'stars': int})
+
+    # randomly sample from each class based on how many samples the minority class holds
+    seed = 42
+    minority_class_count = len(review_df[review_df.loc[:, ('stars')] == 2])
+    review_df = review_df.groupby('stars').apply(lambda x: x.sample(n=minority_class_count, random_state=seed)).reset_index(drop=True)
+    return review_df
+
+def should_run_eval(total_steps, freq, current_step):
+    return current_step % (total_steps // freq) == 0
+
+def eval(model, val_data):
+    # TODO: add some kind of confusion matrix
+    pbar = tqdm(range(len(val_data)))
+    metric = evaluate.load("accuracy")
+    model.eval()
+    for batch in val_data:
+        batch = {
+            "input_ids": batch["input_ids"].to(model.device),
+            "labels": batch["labels"].to(model.device),
+            "attention_mask": batch["attention_mask"].to(model.device),
+        }
+        with torch.no_grad():
+            outputs = model(**batch)
+
+        # compute loss
+        logits = outputs.logits
+        preds = torch.argmax(logits, dim=1)
+        metric.add_batch(predictions=preds, references=batch["labels"])
+        pbar.update(1)
+    acc_result = metric.compute()
+    print(f"Accuracy: {acc_result['accuracy']}")
+
+def save_model(model, outpath, current_epoch, current_step):
+    print(f"saving model at epoch: {current_epoch}, step: {current_step}")
+    outpath += f"/epoch_{current_epoch}/step_{current_step}"
+    model.save_pretrained(outpath)
+
+def train_model(model, epochs, train_dataloader, val_dataloader, train_steps, optimizer, lr_scheduler):
+    pbar = tqdm(range(train_steps))
+
+    run_id = str(uuid.uuid4())
+    output_dir = f"./outputs/bert/{run_id}"
+    model.train()
+    for epoch in range(epochs):
+        current_epoch = epoch + 1
+
+        for step, batch in enumerate(train_dataloader):
+            current_step = step + 1
+            pbar.set_description(f"Epoch {current_epoch} :: Step {current_step}")
+
+            batch = {
+                "input_ids": batch["input_ids"].to(model.device),
+                "labels": batch["labels"].to(model.device),
+                "attention_mask": batch["attention_mask"].to(model.device),
+            }
+
+            # forward
+            outputs = model(**batch)
+            loss = outputs.loss
+
+            # backward
+            loss.backward()
+
+            # update weights
+            optimizer.step()
+            lr_scheduler.step()
+
+            # evaluate and save model
+            if should_run_eval(len(train_dataloader), 2, current_step):
+                eval(model, val_dataloader)
+                save_model(model, output_dir, current_epoch, current_step)
+                model.train()
+            pbar.update(1)
+
+    save_model(model, tokenizer, output_dir, current_epoch, "final")
+
+
+
+
+
+
+
+if __name__ == '__main__':
+
+    # load data
+    print("loading data...\n")
+    path = "yelp_academic_dataset_review.json"
+    df = load_and_prepare_data(path, False)
+
+    # create dataset
+    tokenizer = AutoTokenizer.from_pretrained('bert-base-cased', use_fast=False)
+    review_dataset = ReviewDataSet(tokenizer, df)
+
+    # train and val split
+    train_size = int(0.8 * len(review_dataset))
+    val_size = len(review_dataset) - train_size
+    generator = torch.Generator().manual_seed(42)
+    train_dataset, val_dataset = torch.utils.data.random_split(review_dataset, [train_size, val_size], generator)
+    print(f'train and validation dataset sizes: {len(train_dataset), len(val_dataset)}\n')
+
+    # dataloader
+    train_dataloader = DataLoader(train_dataset, shuffle=True, batch_size=16)
+    val_dataloader = DataLoader(val_dataset, batch_size=16)
+
+    # initialize model
+    model = AutoModelForSequenceClassification.from_pretrained('bert-base-cased', num_labels=5)
+    optimizer = AdamW(model.parameters(), lr=5e-5)
+    num_params = sum([p.numel() for p in model.parameters()])
+    print(f"model params: {num_params}\n")
+
+    epochs = 3
+    train_steps = epochs * len(train_dataloader)
+    lr_scheduler = get_scheduler(name='linear', optimizer=optimizer, num_warmup_steps=0, num_training_steps=train_steps)
+
+    # set device and send model to device
+    device = torch.device('cpu')
+    if torch.cuda.is_available():
+        device = torch.device('cuda')
+    model.to(device)
+
+    print("beginning model training...\n")
+    train_model(model, epochs, train_dataloader, val_dataloader)
+
+    print("complete")
